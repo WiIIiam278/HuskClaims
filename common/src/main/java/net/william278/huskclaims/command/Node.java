a76b6b17e35162d81814cfb21bb69ed3050c5ee5
William <will27528@gmail.com>
2024-01-03 16:58:25 +0000

    refactor: Fix colons in importer keyValue parsing

--- common/src/main/java/net/william278/huskclaims/command/Node.java
+++ common/src/main/java/net/william278/huskclaims/command/Node.java
@@ -106,8 +106,8 @@ public abstract class Node implements Executable {
         final List<String> list = parseStringList(args, index);
         final Map<String, String> map = Maps.newHashMap();
         for (String s : list) {
-            final String[] split = s.split(":");
-            if (split.length == 2) {
+            final String[] split = s.split(":", 2);
+            if (split.length >= 2) {
                 map.put(split[0], split[1]);
             }
         }
